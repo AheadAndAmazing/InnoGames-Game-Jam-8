abd76fa83c7500ae7ea2c80fa09be5a9bdab5e0a
Jon Jelinski <jon_jelinski@t-online.de>
2015-05-30 09:05:53 +0200

    Weltbeschraenkung

--- BuggyPhysics/Assets/Scripts/Movement.cs
+++ BuggyPhysics/Assets/Scripts/Movement.cs
@@ -15,7 +15,13 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		rigid.AddForce (Input.GetAxis("Horizontal")*0.1f, 0.0f, 0.0f, ForceMode.VelocityChange);
+		speedFactor = Input.GetAxis("Horizontal")*0.1f;
+		if (this.transform.position.x > 7.0f && speedFactor > 0.0f) {
+			speedFactor = 0.0f;
+		} else if (this.transform.position.x < -7.0f && speedFactor < 0.0f) {
+			speedFactor = 0.0f;
+		}
+		rigid.AddForce (speedFactor, 0.0f, 0.0f, ForceMode.VelocityChange);
 		//this.transform.Translate (50* (Input.GetAxis ("Horizontal") * Time.deltaTime), 0.0f, Input.GetAxis("Vertical")*Time.deltaTime);
 	}
 }

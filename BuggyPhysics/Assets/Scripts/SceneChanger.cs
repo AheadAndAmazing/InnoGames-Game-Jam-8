09bc2dd7d9b1c4b93d7023603857841a92b9fc3b
Jon Jelinski <jon_jelinski@t-online.de>
2015-05-30 04:34:26 +0200

    Main Menu Prototyp

---
+++ BuggyPhysics/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	public void StartGame()
+	{
+		Application.LoadLevel("Scene1");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+
+	public void StartCredits()
+	{
+		Application.LoadLevel("Credits");
+	}
+}
+
+

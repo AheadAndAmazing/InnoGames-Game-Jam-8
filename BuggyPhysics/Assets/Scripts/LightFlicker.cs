8e856da3cb708d8b66c438a565ec70ed09017b68
Fabian <fabianmueller5@gmx.net>
2015-05-31 00:05:48 +0200

    Scene is fancy now: Mini Shrooms, Grass, Ambient lighting, big shrooms

---
+++ BuggyPhysics/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	Light myLight;
+
+	// Use this for initialization
+	void Start () {
+		myLight = GetComponent<Light> ();
+		myLight.intensity = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myLight.intensity = Random.Range(0.8f,1.0f);
+	}
+}

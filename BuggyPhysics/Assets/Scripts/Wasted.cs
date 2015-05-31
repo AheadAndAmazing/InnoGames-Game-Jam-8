53ac0dc10acd296149d32287dfbe2badda99129b
Fabian <fabianmueller5@gmx.net>
2015-05-31 12:18:52 +0200

    Wasted reworked, Pickup new

--- BuggyPhysics/Assets/Scripts/Wasted.cs
+++ BuggyPhysics/Assets/Scripts/Wasted.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class Wasted : MonoBehaviour {
 
-	void OnCollisionEnter(Collision collision)
+	void OnTriggerEnter(Collider collision)
 	{
 		print ("COLLISION");
-		print (collision.collider.name);
+		//print (collision.collider.name);
 		if(collision.gameObject.tag == "Player")
 		{
 			Rigidbody rg = collision.gameObject.GetComponent<Rigidbody>();
 			rg.constraints = RigidbodyConstraints.None;
-			rg.AddExplosionForce(100, rg.transform.position, 20.0f);
+			//rg.AddExplosionForce(100, rg.transform.position, 20.0f);
+			rg.AddForce(Vector3.up * 100.0f);
+			rg.AddTorque(new Vector3(Random.Range(0,100),0.0f,0.0f));
 		}
 	}
 

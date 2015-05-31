5b3b9de57b33cc96df3144812ed1955e81e20188
Jon Jelinski <jon_jelinski@t-online.de>
2015-05-31 05:46:59 +0200

    Implemented Splattereffect when the Larvae hit the Ground

--- BuggyPhysics/Assets/Scripts/HandleEggCollision.cs
+++ BuggyPhysics/Assets/Scripts/HandleEggCollision.cs
@@ -3,21 +3,34 @@ using System.Collections;
 
 public class HandleEggCollision : MonoBehaviour {
 
+	public GameObject bugSplatter;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Kugel") {
 			other.transform.parent = this.transform;
+
+
+			if(!other.attachedRigidbody.isKinematic)
+			{
+				GameObject currentSplatter = Instantiate(bugSplatter) as GameObject;
+				currentSplatter.transform.parent = other.transform;
+				currentSplatter.transform.localPosition = Vector3.zero;
+			}
 			other.attachedRigidbody.isKinematic = true;
+
+
 		}
 	}
 }
